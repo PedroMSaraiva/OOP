68a139be410da930ea4d71b7a9f2cf8d67080e4b
PedroMSaraiva <pedro.saraiva@protonmail.com>
2024-01-19 17:09:33 -0300

    adding more comments

--- FreioCarro.py
+++ FreioCarro.py
@@ -4,11 +4,13 @@ from abc import ABC, abstractmethod
 class TipoFreiar(ABC):
     @abstractmethod
     def freiar(self, velocidade_de_freio: float):
+        #Método abstrato para definir a estratégia de frenagem.
         pass
     
 # Concrete Strategy 1
 class Freio(TipoFreiar):
     def freiar(self, velocidade_de_freio: float):
+        #Implementação da estratégia de frenagem normal
         return self.freiar_normal(velocidade_de_freio)
 
     def freiar_normal(self, velocidade_de_freio: float):
@@ -18,6 +20,7 @@ class Freio(TipoFreiar):
 class FreioABS(TipoFreiar):
 
     def freiar(self, velocidade_de_freio: float):
+        #Implementação da estratégia de frenagem com ABS.
         return self.freiar_eficiente(velocidade_de_freio)
 
     def freiar_eficiente(self, velocidade_de_freio: float):
@@ -29,21 +32,24 @@ class Carro:
         self._tipo_freiar = tipo_freiar
 
     def freiar(self, velocidade_de_freio: float):
+        #Método para realizar a frenagem usando a estratégia atual.
         self._tipo_freiar.freiar(velocidade_de_freio)
 
-    @tipo_freiar.getter
+    @property
     def tipo_freiar(self) -> TipoFreiar:
+        #Getter para o tipo de frenagem.
         return self._tipo_freiar
 
     @tipo_freiar.setter
     def tipo_freiar(self, tipo_freiar: TipoFreiar):
+        #Setter para alterar o tipo de frenagem.
         self._tipo_freiar = tipo_freiar
         
 # Client code
 if __name__ == "__main__":
     # O carro pode mudar de estratégia em tempo de execução.
     carro = Carro(Freio())
-    carro.freiar(5.0)
+    carro.freiar(5.0) # Uso da estratégia inicial (Freio normal)
 
-    carro._tipo_freiar = FreioABS()
-    carro.freiar(7.0)
+    carro._tipo_freiar = FreioABS() # Mudança de estratégia para FreioABS
+    carro.freiar(7.0) # Uso da nova estratégia (FreioABS)
